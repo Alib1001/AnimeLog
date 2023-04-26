9faa4452b7328f282ac26b3df77f0dc2b5f9121a
Alib281 <superalibek123@gmail.com>
2023-04-26 16:51:33 +0600

    BackupFragment updated

--- app/src/main/java/com/alib/myanimelist/ui/Backup/BackupFragment.java
+++ app/src/main/java/com/alib/myanimelist/ui/Backup/BackupFragment.java
@@ -1,5 +1,11 @@
 package com.alib.myanimelist.ui.Backup;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,38 +13,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.alib.myanimelist.Database.AnimeDatabaseHelper;
 import com.alib.myanimelist.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BackupFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
 public class BackupFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final int REQUEST_CODE = 100;
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public BackupFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BackupFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static BackupFragment newInstance(String param1, String param2) {
         BackupFragment fragment = new BackupFragment();
         Bundle args = new Bundle();
@@ -60,7 +59,70 @@ public class BackupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_backup, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_backup, container, false);
+
+        AnimeDatabaseHelper dbHelper = new AnimeDatabaseHelper(getContext());
+
+        Button importBtn = rootView.findViewById(R.id.importBtn);
+        Button exportBtn = rootView.findViewById(R.id.exportBtn);
+
+        exportBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbHelper.exportDataToJSON();
+                dbHelper.exportDataToTxt();
+                Toast.makeText(getContext(), "Data exported", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        importBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*"); // Set the type of files to be selected, you can customize this as needed
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                startActivityForResult(Intent.createChooser(intent, "Select File"), REQUEST_CODE);
+
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            Uri sourceUri = data.getData();
+
+            ContentResolver contentResolver = getContext().getContentResolver();
+
+            try {
+                InputStream inputStream = contentResolver.openInputStream(sourceUri);
+                File destinationDirectory = new File(getContext().getFilesDir(), "anime_data_json"); // Specify the destination directory
+                destinationDirectory.mkdirs(); // Create the directory if it doesn't exist
+                File destinationFile = new File(destinationDirectory, "anime_data.json"); // Specify the destination file
+                OutputStream outputStream = new FileOutputStream(destinationFile);
+
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+
+                inputStream.close();
+                outputStream.close();
+
+                // File has been successfully imported, update the database
+                Toast.makeText(getContext(),destinationFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getContext(), "Failed to import data", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
